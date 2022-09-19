5b53aabfb73f8e84b6c2cea0c37066e1d1e0a4c9
Chandler Bursey-Reece <cburseyreece@Chandlers-MacBook-Pro.local>
2022-09-19 15:10:23 -0400

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "healthforumec12af95": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
